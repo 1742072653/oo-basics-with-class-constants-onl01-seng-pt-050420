ea82889c020ed542e7c2ae76d8fc09cd4cd36f61
Wenhao Guo <gwh1742072653@gmail.com>
2020-05-26 17:59:02 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,7 @@
 class Book
   attr_accessor :author, :page_count
   attr_reader :title,:genre
-
+  GENRES=[]
   def initialize(title)
     @title = title
   end
@@ -12,5 +12,6 @@ class Book
   
   def genre=(genre)
     @genre = genre
-    
+    GENRES << genre
+  end
 end
